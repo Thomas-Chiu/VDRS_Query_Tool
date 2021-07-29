2e5e291c8af7eea3bf00c675658b3471f346c4f9
Thomas <josemich0511@gmail.com>
2021-07-29 13:52:22 +0800

    50server test

--- init.php
+++ init.php
@@ -4,4 +4,4 @@ require_once("./components/navbar.php");
 require_once("./components/inputGroup.php");
 require_once("./components/table.php");
 require_once("./components/loader.php");
-require_once("./components/goTop.php");
+// require_once("./components/goTop.php");
